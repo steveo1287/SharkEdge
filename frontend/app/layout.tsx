c460be2f95367c815325f55857ee6355e55c44ea
steveo1287 <steveo1287@yahoo.com>
2026-03-22 18:19:00 -0500

    Create RootLayout component with metadata

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Shark Odds"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
