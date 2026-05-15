15e0e5bf2bdbca08f16eaa108dad3e38a7438dbd
steveo1287 <steveo1287@yahoo.com>
2026-05-15 08:09:18 -0500

    Route UFC league page to Fight Lab

--- app/leagues/ufc/page.tsx
+++ app/leagues/ufc/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 export default function UfcLeaguePage() {
-  redirect("/sharkfights/ufc");
+  redirect("/sim/ufc");
 }
